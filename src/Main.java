dc99bc2185e722f1503e6a15e57dbeabbfe3f3fe
raultorres1996-coder <raultorres1996@tec.mx>
2026-06-26 15:19:08 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
+// Clase principal de la aplicación
 public class Main {
 
     public static void main(String[] args) {
 
+        // Creación de dos objetos de tipo Estudiante
         Estudiante estudiante1 =
                 new Estudiante("Juan", 20, "Ingeniería");
 
         Estudiante estudiante2 =
                 new Estudiante("María", 22, "Sistemas");
 
+        // Mostrar la información del primer estudiante
         estudiante1.mostrarInformacion();
 
         System.out.println("----------------");
 
+        // Mostrar la información del segundo estudiante
         estudiante2.mostrarInformacion();
     }
 }
