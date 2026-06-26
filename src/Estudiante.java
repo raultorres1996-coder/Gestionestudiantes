4e1a153f8b46eed1e702fe5f5ecadf7d5ef10341
raultorres1996-coder <raultorres1996@tec.mx>
2026-06-26 15:18:45 -0600

    Update Estudiante.java

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,15 +1,19 @@
+// Clase que representa a un estudiante
 public class Estudiante {
 
+    // Atributos del estudiante
     String nombre;
     int edad;
     String carrera;
 
+    // Constructor que inicializa los datos del estudiante
     public Estudiante(String nombre, int edad, String carrera) {
         this.nombre = nombre;
         this.edad = edad;
         this.carrera = carrera;
     }
 
+    // Método que muestra la información del estudiante en la consola
     public void mostrarInformacion() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Edad: " + edad);
